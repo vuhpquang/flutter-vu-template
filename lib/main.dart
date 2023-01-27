02a9474a148dc92dcdf1779d1968258b5c12d369
Ha Phung Quang Vu <quangvupp321@gmail.com>
2023-01-27 23:11:15 +0700

    feat: init from boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,28 @@
+import 'dart:async';
+
+import 'package:boilerplate/ui/my_app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_boilerplate/provider/counter_provider.dart';
-import 'package:flutter_boilerplate/screens/color_page.dart';
-import 'package:flutter_boilerplate/screens/hero_page.dart';
-import 'package:flutter_boilerplate/screens/home_page.dart';
-import 'package:flutter_boilerplate/screens/search_page.dart';
-import 'package:flutter_boilerplate/screens/second_page.dart';
-import 'package:flutter_boilerplate/screens/settings_page.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  // runApp(const MyApp());
+import 'di/components/service_locator.dart';
 
-  // runApp(ChangeNotifierProvider(
-  //   create: (_context) => CounterProvider(),
-  //   child: MyApp(),
-  // ));
-
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider<CounterProvider>(create: (_) => CounterProvider()),
-    ],
-    child: MyApp(),
-  ));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setPreferredOrientations();
+  await setupLocator();
+  return runZonedGuarded(() async {
+    runApp(MyApp());
+  }, (error, stack) {
+    print(stack);
+    print(error);
+  });
 }
 
-class Example {
-  Future<String> _getIPAddress() {
-    final url = Uri.https('httpbin.org', '/ip');
-    return http.get(url).then((response) {
-      String ip = jsonDecode(response.body)['origin'];
-      return ip;
-    });
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        brightness: Brightness.light,
-        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.amber),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      routes: <String, WidgetBuilder>{
-        '/home': (BuildContext context) =>
-            const MyHomePage(title: 'Flutter Demo Home Page'),
-        '/settings': (BuildContext context) => const SettingsPage(),
-        '/search': (BuildContext context) => const SearchPage(),
-        '/second': (BuildContext context) => const SecondPage(),
-        '/color': (BuildContext context) => ColorPage(),
-        '/hero': (BuildContext context) => HeroPage(),
-      },
-      // home: TestWidget(),
-    );
-  }
+Future<void> setPreferredOrientations() {
+  return SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
 }
